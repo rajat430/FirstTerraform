74464b195b5cb9a6b415d3a0ae23847638295faf
rajatbhawsar <rajatbhawsar@KTRAININD.COM>
2025-05-05 18:53:48 +0530

    first commit

--- variables.tf
+++ variables.tf
@@ -2,16 +2,5 @@ variable "subscription_id" {
   default = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
 }
 variable "workspace" {
-  default = "dit"
-}
-
-variable "client_id" {
-  
-}
-
-variable "client_secret" {
-  
-}
-variable "tenant_id" {
-  
+  default = "dit-cli"
 }
