35f0ee0a541a3e66a032528b63b71579529ad788
rajat430 <47021948+rajat430@users.noreply.github.com>
2025-05-05 23:35:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,75 +1,39 @@
-data "azurerm_resource_group" "example" {
-  name     = "1-68a120af-playground-sandbox"
+
+# data "azurerm_resource_group" "example" {
+#     name = var.vnet_resource_group
+# }
+
+data "azurerm_resource_group" "vnet_rg" {
+ name =  var.vnet_resource_group
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "rajatstorageacc0120"
-  resource_group_name      = data.azurerm_resource_group.example.name
-  location                 = data.azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_network_security_group" "example" {
+  for_each = var.subnet_details
+  name                = each.value.nsg_name
+  location            = data.azurerm_resource_group.vnet_rg.location
+  resource_group_name = data.azurerm_resource_group.vnet_rg.name
 
-  tags = {
-    environment = "staging"
-  }
+  security_rule = var.subnet_details.value.nsg_rules
+
 }
 
-module "network" {
-  source  = "app.terraform.io/rajat430/network/azure"
-  version = "1.0.0"
-  # insert required variables here
-  vnet_resource_group= data.azurerm_resource_group.example.name
-  dns_servers = [ "10.0.0.4","10.0.0.5" ]
-  subnet_details = {
-    subnet1 = {
-      name="subnet1",
-      address_prefixes=["10.0.0.0/29"],
-      nsg_name="nsg001",
-      nsg_rules={
-         "rule1":{
-          name="rule1"
-          properties={
-            protocol="*",
-            sourcePortRange= "*"
-            destinationPortRange= "8090"
-            sourceAddressPrefix="*"
-            destinationAddressPrefix= "*"
-            access= "Allow"
-            priority= 1002
-            direction= "Inbound"
-            sourcePortRanges= []
-            destinationPortRanges= []
-            sourceAddressPrefixes= []
-            destinationAddressPrefixes= []
-          }
-        }
-      }
-    },
-    subnet2={
-      name="subnet2",
-      address_prefixes=["10.0.0.8/29"],
-      nsg_name="nsg002",
-      nsg_rules={
-        "rule1":{
-          name="rule1"
-          properties={
-            protocol="*",
-            sourcePortRange= "*"
-            destinationPortRange= "8090"
-            sourceAddressPrefix="*"
-            destinationAddressPrefix= "*"
-            access= "Allow"
-            priority= 1002
-            direction= "Inbound"
-            sourcePortRanges= []
-            destinationPortRanges= []
-            sourceAddressPrefixes= []
-            destinationAddressPrefixes= []
-          }
-        }
-      }
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.vnet_rg.location
+  resource_group_name = data.azurerm_resource_group.vnet_rg.name
+  address_space       = var.vnet_address_space
+  dns_servers         = var.dns_servers
+
+  dynamic "subnet" {
+    for_each = var.subnet_details 
+    content {
+      name = subnet.value.name
+      address_prefixes = subnet.value.address_prefixes
+      security_group = azurerm_network_security_group.example[subnet.key].id
     }
   }
-  vnet_address_space = [ "10.0.0.0/24" ]
-  vnet_name = "vnet001"
+
+  tags = {
+    environment = "Production"
+  }
 }
