dee0ead5eff936f35b861114e99d5ef5986cd82a
rajatbhawsar <rajatbhawsar@KTRAININD.COM>
2025-05-05 18:54:51 +0530

    first commit

--- providers.tf
+++ providers.tf
@@ -18,8 +18,5 @@ terraform {
 provider "azurerm" {
   features {}
     resource_provider_registrations = "none"
-      client_id       = var.client_id
-      client_secret   = var.client_secret
-      tenant_id       = var.tenant_id
       subscription_id = var.subscription_id
 }
